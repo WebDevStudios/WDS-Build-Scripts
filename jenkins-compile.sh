fb1bd01a03b442c25bb80d7460451622c09bc848
Jay Wood <jerry@webdevstudios.com>
2017-08-22 08:38:56 -0400

    Allow BUILD_FILES to not be set

--- jenkins-compile.sh
+++ jenkins-compile.sh
@@ -1,16 +1,17 @@
 #!bin/bash
 
-# This script iterates over BUILD_FILES and
+# This script iterates over BUILD_FILES -- if set, and
 # runs the build.sh script for those folders.
+if [[ -n "${BUILD_FILES+set}" ]]; then
+	. "${WORKSPACE}/jenkins-config.sh"
 
-. "${WORKSPACE}/jenkins-config.sh"
+	message "Build from $DIR";
 
-message "Build from $DIR";
+	for i in "${BUILD_FILES[@]}"; do :
 
-for i in "${BUILD_FILES[@]}"; do :
+		# Build the app directories
+		message "Executing build for $DIR/$i"
+		sh "$DIR/build.sh" "$DIR/$i"
 
-	# Build the app directories
-	message "Executing build for $DIR/$i"
-	sh "$DIR/build.sh" "$DIR/$i"
-
-done
+	done
+fi
